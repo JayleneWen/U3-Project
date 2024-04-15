2e946413fd8e48071d42c6f2d1c8600d8c7d0ef9
Jaylene Wen <146998451+JayleneWen@users.noreply.github.com>
2024-04-15 22:43:55 +0000

    added buttons

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ let optionOneEnd = document.querySelector(".option-one-end");
 let optionTwoEnd = document.querySelector(".option-two-end");
 let button2 = document.querySelector(".button2");
 let button3 = document.querySelector(".button3");
+let button4 = document.querySelector(".button4");
+let option1One = document.querySelector(".option1-one");
+let option1Two = document.querySelector(".option1-two");
+let optionAnswerThePhoneCallScreen = document.querySelector( ".option-Answer-the-phone-call-screen");
+let option1Three = document.querySelector(".option1-three")
 
 
 
@@ -20,19 +25,47 @@ let button3 = document.querySelector(".button3");
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
-
+// let option1TwoButton = document.querySelector(".option1-two");
 optionOne.addEventListener('click', function() {
   storyOpening.style.display = "none";
   optionOneScreen.style.display = "block";
-button2.style.display = "block";
+  button2.style.display = "block";
 });
 
 optionTwo.addEventListener('click', function() {
   storyOpening.style.display = "none";
   optionTwoScreen.style.display = "block";
   button3.style.display = "block";
+  });
 
+option1One.addEventListener('click', function() {
+  storyOpening.style.display = "none";
+  optionAnswerThePhoneCallScreen.style.display = "block";
+  optionTwoScreen.style.display = "none";
+  optionOneScreen.style.display = "none";
+  button4.style.display = "block";
 });
+
+option1Two.addEventListener('click', function() {
+  storyOpening.style.display = "none";
+  optionOneEnd.style.display = "block";
+  optionTwoScreen.style.display = "none";
+  optionOneScreen.style.display = "none";
+  button4.style.display = "block";
+});
+ 
+
+  option1Three.addEventListener('click', function() {
+    storyOpening.style.display = "none";
+   
+    optionAnswerThePhoneCallScreen.style.display = "none";
+    optionTwoScreen.style.display = "none";
+    optionOneScreen.style.display = "none";
+    optionOneEnd.style.display = "none";
+    button4.style.display = "none";
+    optionTwoEnd.style.display = "block";
+    
+  });
 
 // optionThree.addEventListener('click', function(){
 
